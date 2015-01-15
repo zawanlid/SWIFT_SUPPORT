b3ab62db168234551f4589e1db1d847cc573ebe7
Dil Nawaz <dilnawaz1985@hotmail.com>
2015-01-15 13:38:56 +0800

    buildeing dynamic query

---
+++ src/main/java/org/dnawaz/constant/Constant.java
@@ -0,0 +1,36 @@
+package org.dnawaz.constant;
+
+public interface Constant {
+
+	String SOURCE_SYSTEM_NOVA = "NOVA";
+	String EVENT_NAME_NOVA = "SwiftNovaUpdateCTT";
+	
+	String SOURCE_SYSTEM_ICP = "ICP";
+	String EVENT_NAME_ICP = "evUpdateTRfrSWF";
+	
+	
+	/**
+	 * 
+	 */
+	String DATE_FORMAT_DDMMYYYY = "ddMMyyyy";
+	
+	/**
+	 * 
+	 */
+	String DATE_FORMAT_DDMMYYYY2 = "dd/MM/yyyy";
+	
+	/**
+	 * 
+	 */
+	String DATE_FORMAT_YYYYMMDD = "yyyyMMdd";
+	/**
+	 * 
+	 */
+	String DATE_FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
+	/**
+	 * 
+	 */
+	String DATE_FORMAT_TIMESTAMP = "yyyyMMddHHmmss";
+	
+
+}
